b21f5c0554b6e4823471e2793b862cb2d34510bc
Lucas <lucasprepara12@gmail.com>
2025-05-07 14:07:18 -0300

    Importing and using the list clients card

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/components/header";
 import { UserForm } from "@/components/user-form";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { ClientCard } from "@/components/client-card";
 
 export default async function WelcomePage() {
   const session = await auth.api.getSession({ headers: await headers() });
@@ -17,10 +18,11 @@ export default async function WelcomePage() {
         ) : (
           <h1>Bem vindo, {session?.user.username}!</h1>
         )}
+        <div className="self-start flex flex-col p-4 gap-y-4 w-full">
+          <ClientForm userId={session?.user.id || ""} />
+          <ClientCard />
+        </div>
       </main>
-      <div className="p-6">
-        <ClientForm userId={session?.user.id || ""} />
-      </div>
     </>
   );
 }
