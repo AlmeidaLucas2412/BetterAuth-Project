4cf9ee1b1076bc3f198fe5ed429fd95fd85316aa
Lucas <lucasprepara12@gmail.com>
2025-05-08 14:28:04 -0300

    Creates a new route (first-access) to handle new user register.

---
+++ app/first-access/page.tsx
@@ -0,0 +1,9 @@
+import { UserForm } from "@/components/user-form";
+
+export default function FirstAccess() {
+  return (
+    <section className="h-dvh flex flex-col items-center justify-center">
+      <UserForm />
+    </section>
+  );
+}
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,23 +1,21 @@
 import { ClientForm } from "@/components/client-form";
 import { Header } from "@/components/header";
-import { UserForm } from "@/components/user-form";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { ClientCard } from "@/components/client-card";
+import { redirect } from "next/navigation";
 
 export default async function WelcomePage() {
   const session = await auth.api.getSession({ headers: await headers() });
   const isFirstAccess = !session?.user.username;
 
+  if (isFirstAccess) redirect("/first-access");
+
   return (
     <>
       <Header />
       <main className="flex flex-col items-center">
-        {isFirstAccess ? (
-          <UserForm />
-        ) : (
-          <h1>Bem vindo, {session?.user.username}!</h1>
-        )}
+        <h1>Bem vindo, {session?.user.username}!</h1>
         <div className="self-start flex flex-col p-4 gap-y-4 w-full">
           <ClientForm userId={session?.user.id || ""} />
           <ClientCard />
